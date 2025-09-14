b2e605eea8556d3726455c7098e9e74e57f82da5
Sumit-kj <sumitkj7.5@gmail.com>
2025-09-14 18:07:24 +0530

    Added validation for insert, udpate and delete query

--- common/constants.py
+++ common/constants.py
@@ -21,27 +21,61 @@ c_resp_syntax_validation_failure = 'Invalid SQL Query'
 c_str_query = 'query'
 
 # Regex Patterns
-c_rp_sql_query_select = '^SELECT\s+[\*\w,\s]+'
-c_rp_sql_query_from = '\s+FROM\s+\w+'
-c_rp_sql_query_basic = f'{c_rp_sql_query_select}{c_rp_sql_query_from}'
-c_rp_sql_query_join = '\s+JOIN\s+\w+\s+'
-c_rp_sql_query_on = '\s+ON\s+.+'
-c_rp_sql_query_join_on = f'{c_rp_sql_query_join}{c_rp_sql_query_on}'
+c_rp_sql_query_table = '\w+'
 c_rp_sql_query_where = '\s+WHERE\s+.+'
-c_rp_sql_query_group_by = '\s+GROUP\s+BY\s+.+'
-c_rp_sql_query_having = '\s+HAVING\s+.+'
-c_rp_sql_query_order_by = '\s+ORDER\s+BY\s+.+'
-c_rp_sql_query_limit = '\s+LIMIT\s+\d+'
-c_rp_sql_query_offset = '\s+OFFSET\s+\d+'
 c_rp_sql_query_semi_colon = '\s*;?\s*$'
-c_rp_sql_query = (
-    fr"{c_rp_sql_query_basic}"
-    fr"({c_rp_sql_query_join_on})?"
-    fr"({c_rp_sql_query_where})?"
-    fr"({c_rp_sql_query_group_by})?"
-    fr"({c_rp_sql_query_having})?"
-    fr"({c_rp_sql_query_order_by})?"
-    fr"({c_rp_sql_query_limit})?"
-    fr"({c_rp_sql_query_offset})?"
-    fr"{c_rp_sql_query_semi_colon}"
+
+c_rp_sql_select_query_select = '^SELECT\s+[\*\w,\s]+'
+c_rp_sql_select_query_from = '\s+FROM\s+\w+'
+c_rp_sql_select_query_basic = f'{c_rp_sql_select_query_select}{c_rp_sql_select_query_from}'
+c_rp_sql_select_query_join = '\s+JOIN\s+\w+\s+'
+c_rp_sql_select_query_on = '\s+ON\s+.+'
+c_rp_sql_select_query_join_on = f'{c_rp_sql_select_query_join}{c_rp_sql_select_query_on}'
+c_rp_sql_select_query_group_by = '\s+GROUP\s+BY\s+.+'
+c_rp_sql_select_query_having = '\s+HAVING\s+.+'
+c_rp_sql_select_query_order_by = '\s+ORDER\s+BY\s+.+'
+c_rp_sql_select_query_limit = '\s+LIMIT\s+\d+'
+c_rp_sql_select_query_offset = '\s+OFFSET\s+\d+'
+
+c_rp_sql_insert_query_insert = '^INSERT\s+INTO\s+'
+c_rp_sql_insert_query_columns = '\s*\(([\w\s,]+)\)'
+c_rp_sql_insert_query_values = '\s*VALUES\s*\(.*\)'
+
+c_rp_sql_update_query_update = '\s*UPDATE\s+'
+c_rp_sql_update_query_set = '\s+SET\s+.+'
+
+c_rp_sql_delete_query_delete = '\s*DELETE\s+FROM\s+'
+
+c_rp_sql_select_query = (
+    fr'{c_rp_sql_select_query_basic}'
+    fr'({c_rp_sql_select_query_join_on})?'
+    fr'({c_rp_sql_query_where})?'
+    fr'({c_rp_sql_select_query_group_by})?'
+    fr'({c_rp_sql_select_query_having})?'
+    fr'({c_rp_sql_select_query_order_by})?'
+    fr'({c_rp_sql_select_query_limit})?'
+    fr'({c_rp_sql_select_query_offset})?'
+    fr'{c_rp_sql_query_semi_colon}'
 )
+c_rp_sql_insert_query = (
+    fr'{c_rp_sql_insert_query_insert}'
+    fr'{c_rp_sql_query_table}'
+    fr'{c_rp_sql_insert_query_columns}'
+    fr'{c_rp_sql_insert_query_values}'
+    fr'{c_rp_sql_query_semi_colon}'
+)
+c_rp_sql_update_query = (
+    fr'{c_rp_sql_update_query_update}'
+    fr'{c_rp_sql_query_table}'
+    fr'{c_rp_sql_update_query_set}'
+    fr'({c_rp_sql_query_where})?'
+    fr'{c_rp_sql_query_semi_colon}'
+)
+c_rp_sql_delete_query = (
+    fr'{c_rp_sql_delete_query_delete}'
+    fr'{c_rp_sql_query_table}'
+    fr'{c_rp_sql_query_where}'
+    fr'{c_rp_sql_query_semi_colon}'
+)
+
+
--- validator/syntax_validator.py
+++ validator/syntax_validator.py
@@ -2,8 +2,16 @@ import re
 
 from common import constants as const
 
-def sql_regex_validation(query):
-    pattern = re.compile(const.c_rp_sql_query)
-    if pattern.match(query):
+def sql_regex_validation(query) -> bool:
+    """
+    This function performs a regex validation of a SQL query
+    :param query: The query to be validated
+    :return: True if the SQL matches the regex, else False
+    """
+    select_pattern = re.compile(const.c_rp_sql_select_query)
+    insert_pattern = re.compile(const.c_rp_sql_insert_query)
+    update_pattern = re.compile(const.c_rp_sql_update_query)
+    delete_pattern = re.compile(const.c_rp_sql_delete_query)
+    if select_pattern.match(query) or insert_pattern.match(query) or update_pattern.match(query) or delete_pattern.match(query):
         return True
     return False
